95eac4be6281513613ae2829fa2b3171d531de88
Lava Kumar <tinku.lava26@gmail.com>
2024-02-16 12:27:17 +0530

    added dependencies in frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import PastUploads from './Pages/PastUploads';
+import Upload from './Pages/Upload';
+import Account from './Pages/Account';
+import RenderView from './Pages/RenderView'; // Import RenderView
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+// Main App Component with Router
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/past-uploads" element={<PastUploads />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/account" element={<Account />} />
+      <Route path="/render-view/:fileName" element={<RenderView />} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Render App
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
+
 reportWebVitals();
